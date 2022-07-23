639562e1a213c32de3a9f2fe633b78b2353afc0c
unknown <sand8594@hufs.ac.kr>
2022-07-23 17:08:42 +0900

    오류나서 재업로드(~Header 구현)

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter ,Routes, Route} from 'react-router-dom'
+import Header from './Components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
+
+function App() {
+  return (
+    <BrowserRouter /*basename="/netflix-clone/"*/>
+      <Header/>
+      <Routes>
+        <Route path = "/tv" element = {<Tv/>}/>
+        <Route path = "/search" element = {<Search/>}/>
+        <Route path = "/" element = {<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
